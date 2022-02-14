e7e4e5e4d4d17824770ae3f820ab5a3df02d04d4
veldurai <testmrf1@gmail.com>
2022-02-14 11:46:18 +0530

    nothing has done today

--- src/app/instrument/create/create.component.ts
+++ src/app/instrument/create/create.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CrudService } from 'src/app/services/crud.service';
 
 @Component({
@@ -11,7 +11,7 @@ import { CrudService } from 'src/app/services/crud.service';
 export class CreateComponent implements OnInit {
 
   
-  constructor(private _create:CrudService, private router: Router) { }
+  constructor(private _create:CrudService, private router: Router, private route:ActivatedRoute) {}
 
   ngOnInit() {
   }
@@ -29,12 +29,28 @@ data={
     country:"",
   }
 }
+vewpage
  
   onSubmit(myForm: NgForm){
     console.log(myForm)
     this._create.createins(this.data).subscribe(repat=>{
       this.router.navigate(['/view',repat.data._id]);
     })
+  }
+  onviewincreatpage(){
+    this._create.viewcrud(this.onviewincreatpage).subscribe(
+      showinview=>{
+       this.vewpage=showinview.data
+      })
+  }
+  
+  onview(){
+    this.route.paramMap.subscribe(
+      params=>{
+        this.vewpage=params.get('id')
+        this.onviewincreatpage()
+      }
+    )
   }
 }
 
